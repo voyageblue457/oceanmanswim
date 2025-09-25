edd4493e0b3d04350880dc6cf5a832352d6cb2a6
abdurrahim220 <z.abdurrahim5@gmail.com>
2025-09-25 21:27:56 +0600

    error fix

--- sections/MapSection.js
+++ sections/MapSection.js
@@ -1,42 +1,9 @@
-"use client";
-
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
-
-// Fix marker icon issue in Next.js
+"use client"
+import dynamic from "next/dynamic";
 import "leaflet/dist/leaflet.css";
-delete (L.Icon.Default.prototype )._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: "/leaflet/marker-icon-2x.png",
-  iconUrl: "/leaflet/marker-icon.png",
-  shadowUrl: "/leaflet/marker-shadow.png",
-});
-
-const markers = [
-  { id: 1, lat: 40.7128, lng: -74.006, title: "New York" },
-  { id: 2, lat: 48.8566, lng: 2.3522, title: "Paris" },
-  { id: 3, lat: -33.8688, lng: 151.2093, title: "Sydney" },
-];
-
+const Map = dynamic(() => import("./realMap"), { ssr: false });
 export default function MapSection() {
   return (
-    <MapContainer
-      center={[20, 0]}
-      zoom={2}
-      scrollWheelZoom={false} // disable scroll zoom
-      className="w-full h-screen"
-    >
-      {/* OpenStreetMap tiles */}
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/">OSM</a>'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-
-      {markers.map((marker) => (
-        <Marker key={marker.id} position={[marker.lat, marker.lng]}>
-          <Popup>{marker.title}</Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <Map/>
   );
 }
---
+++ sections/realMap.js
@@ -0,0 +1,30 @@
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+export default function realMap() {
+  return (
+    <MapContainer
+      center={[20, 0]}
+      zoom={2}
+      scrollWheelZoom={false}
+      className="w-full h-screen"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/">OSM</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </MapContainer>
+  );
+}
